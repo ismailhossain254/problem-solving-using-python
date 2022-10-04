c47f5c0a1ff2612932005274042aa6af5a9c707c
cipher <sohanh621@gmail.com>
2022-10-05 01:11:49 +0600

    greatest among three numbers

---
+++ sohan4.py
@@ -0,0 +1,9 @@
+num1= int(input('ekta number den vai= '))
+num2= int(input('ekta number den vai= '))
+num3= int(input('ekta number den vai= '))
+if num1>num2 and num1>num3:
+    print('num1 is greatest')
+elif num2>num1 and num2>num3:
+    print('num2 is greatest')
+else:
+    print('num3 is greatest')
