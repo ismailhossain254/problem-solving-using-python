3d7e644bad4cbb7408c3efe7256a770273577041
ismailhossain254 <sohanh621@gmail.com>
2022-11-22 11:37:41 +0600

    Reverse a given number

--- sohan.9.py
+++ sohan.9.py
@@ -1,6 +1,11 @@
-num = int(input("énter a number:"))
+num = int(input('enter a number:'))
 reversed = 0
+x = num
 while num > 0:
-    reversed = (reversed*10) + num % 10
-    num = num//10
+   reversed = (reversed * 10) + num%10
+   num = num // 10
 print(reversed)
+if x == reversed:
+    print("palindrome number")
+else:
+    print(" not a palindrome number")
